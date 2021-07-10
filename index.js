d8806615da4ce68cfcf97aa8a2c2e1eede14e9a1
DESKTOP-H8VL884\levan <levan.dolidze@outlook.com>
2021-07-10 21:44:25 +0400

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+// const directionTitle =document.querySelector('.directionTitle');
+// const aboutUs =document.querySelector('.aboutUs');
+
+
+// aboutUs.addEventListener('click',()=>{
+// //    const replaceWin =  window.location.replace("index.html");
+
+// aboutUs.classList="navActive"
+//     directionTitle.textContent=`EXACTPRO > Company > about us`
+   
+
+    
+// })
+
+
